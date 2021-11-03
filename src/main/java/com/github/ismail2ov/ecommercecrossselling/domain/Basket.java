4cff153f3f8df5b207e4e2cee5098fbbee6957a8
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-03 11:18:49 +0100

    Add Hibernate types lib

--- src/main/java/com/github/ismail2ov/ecommercecrossselling/domain/Basket.java
+++ src/main/java/com/github/ismail2ov/ecommercecrossselling/domain/Basket.java
@@ -1,11 +1,18 @@
 package com.github.ismail2ov.ecommercecrossselling.domain;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
 
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
 @Entity
 @Table(name = "baskets")
 @Data
@@ -19,6 +26,7 @@ public class Basket {
     @Column(nullable = false, unique = true)
     private Long userId;
 
+    @Type(type = "jsonb")
     @Column(columnDefinition = "jsonb")
     private Items items = new Items();
 }
