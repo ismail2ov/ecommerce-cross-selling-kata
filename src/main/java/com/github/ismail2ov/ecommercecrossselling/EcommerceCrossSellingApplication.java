0507aef3c719e68a89d0de90af619bb558aff63b
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-03 08:33:26 +0100

    Create the project

---
+++ src/main/java/com/github/ismail2ov/ecommercecrossselling/EcommerceCrossSellingApplication.java
@@ -0,0 +1,13 @@
+package com.github.ismail2ov.ecommercecrossselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCrossSellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCrossSellingApplication.class, args);
+	}
+
+}
