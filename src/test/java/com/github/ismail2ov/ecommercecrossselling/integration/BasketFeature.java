6ee2014c1e5a172eee80c4a74db59be4c62e4980
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-10 05:33:46 +0100

    Refactor: extract Testcontainers configurations

--- src/test/java/com/github/ismail2ov/ecommercecrossselling/integration/BasketFeature.java
+++ src/test/java/com/github/ismail2ov/ecommercecrossselling/integration/BasketFeature.java
@@ -12,42 +12,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class BasketFeature {
+public class BasketFeature extends BaseTestcontainers {
 
     public static final int USER_ID = 2;
     public static final String BASKET_URL = "/api/users/" + USER_ID + "/basket";
-
-    @Container
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:14")
-            .withUsername("james")
-            .withPassword("bond")
-            .withDatabaseName("ecommerce");
 
     @Autowired
     BasketRepository basketRepository;
 
     @Autowired
     public TestRestTemplate testRestTemplate;
-
-    @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-    }
 
     @BeforeEach
     void setUp() {
--- src/test/java/com/github/ismail2ov/ecommercecrossselling/integration/ProductFeature.java
+++ src/test/java/com/github/ismail2ov/ecommercecrossselling/integration/ProductFeature.java
@@ -10,36 +10,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ProductFeature {
-
-    @Container
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:14")
-            .withUsername("james")
-            .withPassword("bond")
-            .withDatabaseName("ecommerce");
+public class ProductFeature extends BaseTestcontainers {
 
     @Autowired
     ProductRepository productRepository;
 
     @Autowired
     public TestRestTemplate testRestTemplate;
-
-    @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-    }
 
     @AfterEach
     void tearDown() {
